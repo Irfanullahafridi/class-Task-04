5a45a8e8ed53e6a0ae35557e44eb4d8a8fc57c8f
irfanullahafridi <irfanullahafridi046@gmail.com>
2024-09-21 10:05:50 +0500

    update

---
+++ scripting.js
@@ -0,0 +1,33 @@
+// pehla method
+// function Toogle(){
+//     var btn = document.getElementById("btn");
+
+//    var Myclass = document.getElementById("Myclass");
+
+//    Myclass.classList.toggle("night")
+//     // document.body.classList.toggle("night")
+
+// }
+
+// dusra method
+var btn2 = document.getElementById("clickBtn");
+btn2.className='night'
+btn2.textContent="NIGHT"
+function dayNight(){
+// var btn2 = document.getElementById("clickBtn");
+var bg = document.body;
+
+if(bg.className === 'day'){
+    bg.className = 'night';
+    btn2.textContent="Day";
+    btn2.className='day'
+   
+   
+}
+else{
+    bg.className = 'day';
+     btn2.textContent="Night";
+     btn2.className='night'
+    
+}
+}
